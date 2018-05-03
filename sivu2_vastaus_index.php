8b8fc39183ab22aeb7d2261a534aa4f62db61850
hartoi <31921569+hartoi@users.noreply.github.com>
2018-05-03 09:32:31 +0300

    Create sivu2_vastaus_index.php

---
+++ sivu2_vastaus_index.php
@@ -0,0 +1,11 @@
+
+<html>
+<head><title>Luokkaretki</title></head>
+<body>
+<p>TiVi-17k:n unelmien täyttymys</p>
+<p>Unelma on käymässä toteen ensi viikon torstaina
+lähdemme Eeronkuja 3:n pihalta kello 6:00 kohti 
+<a href="https://fi.wikipedia.org/wiki/Laitila">Laitilaa</a></p>
+<img src="http://www.suomenpienpanimot.fi/img/brewery/laitilan-wirvoitusjuomatehdas-main.jpg">
+</body>
+</html>
